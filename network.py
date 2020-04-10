f0891ba5c5a4d4a4e01627964c2548a63d0d84d9
Dwipam Katariya <dwipam@Dwipams-MacBook-Pro.local>
2020-04-10 17:15:59 -0400

    .

--- network.py
+++ network.py
@@ -3,32 +3,41 @@ import tensorflow_probability as tfp
 
 class Network:
 	def __init__(self, x, y, K=50):
+		self.x = x
 		self.layer_1 = tf.layers.dense(x, units=50, activation=tf.nn.tanh, name="layer_1")
 		self.layer_2 = tf.layers.dense(self.layer_1, units=20, activation=tf.nn.tanh)
 		self.layer_2 = tf.layers.dense(self.layer_2, units=20, activation=tf.nn.tanh)
 		self.layer_2 = tf.layers.dense(self.layer_2, units=20, activation=tf.nn.tanh)
 		self.layer_2 = tf.layers.dense(self.layer_2, units=20, activation=tf.nn.tanh, name="layer_2")
 		self.mu = tf.layers.dense(self.layer_2, units=K, activation=None, name="mu")
-		self.var = tf.exp(tf.layers.dense(self.layer_2, units=K, activation=None, name="sigma"))
+		self.var = tf.exp(tf.layers.dense(self.layer_2, units=K, activation=tf.nn.softplus, name="sigma"))
 		self.pi = tf.layers.dense(self.layer_2, units=K, activation=tf.nn.softmax, name="mixing")
 
-		self.likelihood = tfp.distributions.Normal(loc=self.mu, scale=self.var)
-		self.out = self.likelihood.prob(y)
-		self.out = tf.multiply(self.out, self.pi)
-		self.out = tf.reduce_sum(self.out, 1, keepdims=True)
-		self.out = -tf.log(self.out + 1e-10)
-		self.mean_loss = tf.reduce_mean(self.out)
+		# -------------------- Not using Mixture Family ------------------------
+		# self.likelihood = tfp.distributions.Normal(loc=self.mu, scale=self.var)
+		# self.out = self.likelihood.prob(y)
+		# self.out = tf.multiply(self.out, self.pi)
+		# self.out = tf.reduce_sum(self.out, 1, keepdims=True)
+		# self.out = -tf.log(self.out + 1e-10)
+		# self.mean_loss = tf.reduce_mean(self.out)
+
+		# -------------------- Not using Mixture Family ------------------------
+		self.mixture_distribution = tfp.distributions.Categorical(probs=self.pi)
+		self.distribution = fp.distributions.Normal(loc=self.mu, scale=self.var)
+		self.likelihood = tfp.distributions.MixtureSameFamily(
+															mixture_distribution=self.mixture_distribution,
+        													components_distribution=self.distribution)
+		self.log_likelihood = -self.likelihood.log_prob(tf.transpose(y))
+		self.mean_loss = tf.reduce_mean(self.log_likelihood)
+		# ----------------------------------------------------------------------
 
 		self.global_step = tf.Variable(0, trainable=False)
-		self.learning_rate = tf.train.exponential_decay(0.015, self.global_step, 100, .99, staircase=True,)
-		self.train_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.mean_loss,global_step=self.global_step)
+		self.train_op = tf.train.RMSPropOptimizer(learning_rate=0.01, decay=0.8).minimize(self.mean_loss)
 		self.init = tf.global_variables_initializer()
-		self.init_l = tf.local_variables_initializer()
 
 		# Initialize coefficients
 		self.sess = tf.Session()
 		self.sess.run(self.init)
-		self.sess.run(self.init_l)
 
 class Network1Comp:
 	def __init__(self, x, y):
@@ -44,8 +53,6 @@ class Network1Comp:
 		self.mean_loss = tf.reduce_mean(self.out)
 		self.mean_y = self.mu + 0.5 * self.var**2.
 
-		# self.mean_y = self.y_hat
-		# self.mean_loss =  tf.nn.l2_loss(self.y_hat-y)
 		self.rmse = tf.sqrt(tf.reduce_mean(y - self.mean_y)**2.)
 
 		self.global_step = tf.Variable(0, trainable=False)
--- train.py
+++ train.py
@@ -9,7 +9,6 @@ pdf = PdfPages("x.pdf")
 
 # Define Data
 x_ = np.arange(-10., 10., .1)
-#y = x + 0.3 * np.sin(2 * np.pi * x) + np.random.uniform(-0.1, 0.1, size=(n,1)).astype(np.float32)
 y_ = (np.sin(0.5 * x_) * 5.0 + x_ * 0.5).astype(np.float32)
 func = lambda x : np.random.normal(x,x**2./20.,1)
 y = []
@@ -33,34 +32,20 @@ dataset = tf.data.Dataset \
 iter_ = dataset.make_one_shot_iterator()
 x_, y_ = iter_.get_next()
 
-#network = Network(x_,y_,2)
-network1comp = Network1Comp(x_, y_)
+network = Network(x_,y_,1)
 
 best_loss = 1e+10
 for i in range(EPOCHS * (n//BATCH_SIZE)):
-	# _, loss, lr, mu, var, pi = network.sess.run([network.train_op,
-	# 												network.mean_loss,
-	# 												network.learning_rate,
-	# 												network.mu, network.var, network.pi])
-	_2, loss2, rms2, mean_y, x__ = network1comp.sess.run([network1comp.train_op,
-													network1comp.mean_loss,
-													network1comp.rmse,
-													network1comp.mean_y,
-													network1comp.x])
+	_, loss, mu, var, pi, x__ = network.sess.run([network.train_op,
+													network.mean_loss,
+													network.mu, network.var, network.pi,
+													network.x])
 
-	if loss2 < best_loss:
-		best_mean_y = mean_y
+	if loss < best_loss:
+		best_mean_y = mu[:,0]
 		best_x = x__
-		best_loss = loss2
-		# import pdb;pdb.set_trace()
-		# print("X: ",x[0])å
-		# print("Y: ",y[0])
-		# print("Mu: ",mu[0])
-		# print("Var: ",var[0])
-		# print("Pi: ",pi[0])
-		# # pd.DataFrame({'x': x, 'y': y, 'mu': mu, 'sigma': var, 'pi': pi})
-		# print("Epoch: {} Loss: {:3.3f} LR: {}".format(i, loss, lr))
-		print("Epoch: {} Loss: {:3.3f} RMSE {:3.3f}".format(i, loss2, rms2))
+		best_loss = loss
+		print("Epoch: {} Loss: {:3.3f}".format(i, loss))
 
 fig, axs = plt.subplots(1,1)
 plt.plot(x, y, '.')
