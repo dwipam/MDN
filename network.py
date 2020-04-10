90d6d1e8d108c57b83d0e754c807a5278ab5c10b
Dwipam Katariya <dwipam@Dwipams-MacBook-Pro.local>
2020-04-10 14:47:04 -0400

    Make it run with 1 components MDN

--- network.py
+++ network.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 
 class Network:
-	def __init__(self,x,y, K=50):
+	def __init__(self, x, y, K=50):
 		self.layer_1 = tf.layers.dense(x, units=50, activation=tf.nn.tanh, name="layer_1")
 		self.layer_2 = tf.layers.dense(self.layer_1, units=20, activation=tf.nn.tanh)
 		self.layer_2 = tf.layers.dense(self.layer_2, units=20, activation=tf.nn.tanh)
@@ -21,6 +21,35 @@ class Network:
 
 		self.global_step = tf.Variable(0, trainable=False)
 		self.learning_rate = tf.train.exponential_decay(0.015, self.global_step, 100, .99, staircase=True,)
+		self.train_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.mean_loss,global_step=self.global_step)
+		self.init = tf.global_variables_initializer()
+		self.init_l = tf.local_variables_initializer()
+
+		# Initialize coefficients
+		self.sess = tf.Session()
+		self.sess.run(self.init)
+		self.sess.run(self.init_l)
+
+class Network1Comp:
+	def __init__(self, x, y):
+		self.x = x
+		self.layer_1 = tf.layers.dense(x, units=50, activation=tf.nn.softplus)
+		self.layer_2 = tf.layers.dense(self.layer_1, units=20, activation=tf.nn.softplus)
+		self.layer_3 = tf.layers.dense(self.layer_2, units=10, activation=tf.nn.softplus)
+		self.mu = tf.layers.dense(self.layer_3, units=1, activation=None)
+		self.var = tf.layers.dense(self.layer_3, units=1, activation=tf.nn.softplus)
+
+		self.likelihood = tfp.distributions.Normal(loc=self.mu, scale=self.var)
+		self.out = -self.likelihood.log_prob(y)
+		self.mean_loss = tf.reduce_mean(self.out)
+		self.mean_y = self.mu + 0.5 * self.var**2.
+
+		# self.mean_y = self.y_hat
+		# self.mean_loss =  tf.nn.l2_loss(self.y_hat-y)
+		self.rmse = tf.sqrt(tf.reduce_mean(y - self.mean_y)**2.)
+
+		self.global_step = tf.Variable(0, trainable=False)
+		self.learning_rate = tf.train.exponential_decay(0.01, self.global_step, 2000, .9, staircase=False)
 		self.train_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.mean_loss,global_step=self.global_step)
 		self.init = tf.global_variables_initializer()
 		self.init_l = tf.local_variables_initializer()
--- train.py
+++ train.py
@@ -1,31 +1,60 @@
 import random
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-from network import Network
+from network import Network, Network1Comp
 import tensorflow as tf
 pdf = PdfPages("x.pdf")
-n=5000
+n=1000
 
-y = np.random.uniform(0., 1., (n,1)).astype(np.float32)
-x = y + 0.3 * np.sin(2 * np.pi * y) + np.random.uniform(-0.1, 0.1, size=(n,1)).astype(np.float32)
+# Define Data
+x = np.random.uniform(.5, 17.5, (n,1)).astype(np.float32)
+#y = x + 0.3 * np.sin(2 * np.pi * x) + np.random.uniform(-0.1, 0.1, size=(n,1)).astype(np.float32)
+y = np.sin(0.5 * x) * 5.0 + x * 0.5 + np.float32(np.random.normal(size=(n, 1))).astype(np.float32)
 
-fig, axs = plt.subplots(1,1)
-plt.plot(x,y,'.')
-pdf.savefig(fig)
-pdf.close()
-
+# Read into TF dataset
+EPOCHS = 2000
+BATCH_SIZE=n
 dataset = tf.data.Dataset \
     					.from_tensor_slices((x, y)) \
-    					.shuffle(n).repeat().batch(n)
+    					.repeat(EPOCHS).shuffle(n).batch(BATCH_SIZE)
 iter_ = dataset.make_one_shot_iterator()
-x_, y_ = iter_.get_next()
-network = Network(x_,y_,1)
 
-EPOCHS = 100000
+all_ = tf.data.Dataset.from_tensor_slices((x, y)).batch(n)
+x_, y_ = iter_.get_next()
+
+#network = Network(x_,y_,2)
+network1comp = Network1Comp(x_, y_)
+
 best_loss = 1e+10
-for i in range(EPOCHS):
-	_, loss, lr = network.sess.run([network.train_op, network.mean_loss, network.learning_rate])
-	if loss < best_loss:
-		best_loss = loss
-		print "Epoch: {} Loss: {:3.3f} LR: {}".format(i, loss, lr)
+for i in range(EPOCHS * (n//BATCH_SIZE)):
+	# _, loss, lr, mu, var, pi = network.sess.run([network.train_op,
+	# 												network.mean_loss,
+	# 												network.learning_rate,
+	# 												network.mu, network.var, network.pi])
+	_2, loss2, rms2, mean_y, x__ = network1comp.sess.run([network1comp.train_op,
+													network1comp.mean_loss,
+													network1comp.rmse,
+													network1comp.mean_y,
+													network1comp.x])
+
+	if loss2 < best_loss:
+		best_mean_y = mean_y
+		best_x = x__
+		best_loss = loss2
+	# import pdb;pdb.set_trace()
+	# print("X: ",x[0])å
+	# print("Y: ",y[0])
+	# print("Mu: ",mu[0])
+	# print("Var: ",var[0])
+	# print("Pi: ",pi[0])
+	# # pd.DataFrame({'x': x, 'y': y, 'mu': mu, 'sigma': var, 'pi': pi})
+	# print("Epoch: {} Loss: {:3.3f} LR: {}".format(i, loss, lr))
+	print("Epoch: {} Loss: {:3.3f} RMSE {:3.3f}".format(i, loss2, rms2))
+fig, axs = plt.subplots(1,1)
+plt.plot(x, y, '.')
+plt.plot(best_x, best_mean_y, '.')
+pdf.savefig(fig)
+plt.close()
+pdf.close()
